f1a68cb9cb3c5ace380cfff6a03f36d811f2ba46
Jack <wa631016583@gmail.com>
2025-10-27 10:53:59 +0800

    github的bug修复2

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from backend.main import app
+from main import app
 
 client = TestClient(app)
 
@@ -9,7 +9,3 @@ def test_get_projects():
     assert response.status_code == 200
     # 验证返回的是一个JSON数组（列表）
     assert isinstance(response.json(), list)
-
-def test_read_main():
-    response = client.get("/api/projects")
-    assert response.status_code == 200
